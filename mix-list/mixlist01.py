3e0bc23d699e6812eea6414ec56e1e6c9ef80b11
Execute27 <luke.halter27@gmail.com>
2023-07-05 21:33:28 +0000

    Challenge

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -14,3 +14,6 @@ print("The last item in the list (state): " + my_list[2] )
 
 # display only the IP addresses to the screen.
 iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+#addup the strings
+print("IP addresses: " + iplist[3] + ", and " + iplist[4] )
