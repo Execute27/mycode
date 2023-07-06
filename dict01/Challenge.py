f4899e2fd0fb1ee6da35588cd5b37869c5b39ce5
Execute27 <luke.halter27@gmail.com>
2023-07-06 19:47:44 +0000

    learning about dictionaries

---
+++ dict01/Challenge.py
@@ -0,0 +1,28 @@
+#1 Save a user's input to the variable char_name for the following questioin
+#  Which character do you want to know about? (starlord, Mystique, Hul
+
+#char_name
+
+#2 Save a user input to the variable char_stat for the following question:
+#  What statistic do you want to know about? (real name, powers, archenemy)
+
+#char_stat
+
+marvelchars= {
+"Starlord":
+  {"real name": "peter quill",
+  "powers": "dance moves",
+  "archenemy": "Thanos"},
+
+"Mystique":
+  {"real name": "raven darkholme",
+  "powers": "shape shifter",
+  "archenemy": "Professor X"},
+
+"Hulk":
+  {"real name": "bruce banner",
+  "powers": "super strength",
+  "archenemy": "adrenaline"}
+             }
+ <char_name>'s <char_stat> is: <value>
+
